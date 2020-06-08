71e2a353e3098602774c2a19dc14720771326940
Benjamin <caliangben@hotmail.com>
2020-06-07 18:15:46 -0700

    Completed Project 1
    2D Scene has been completed with custom pixel art.

--- P1/SDLProject/main.cpp
+++ P1/SDLProject/main.cpp
@@ -4,36 +4,173 @@
 #include <GL/glew.h>
 #endif
 
+#define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include "glm/mat4x4.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "ShaderProgram.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
-int main(int argc, char* argv[]) {
-	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
-	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
-	SDL_GL_MakeCurrent(displayWindow, context);
+ShaderProgram program;
+glm::mat4 viewMatrix, modelMatrix, projectionMatrix,
+          flying_bug_1_matrix, flying_bug_2_matrix, flying_bug_3_matrix, orange_circle_matrix;
+
+float flying_bug_1_x = 0;
+float flying_bug_1_y = 0;
+float flying_bug_2_x = 0;
+float flying_bug_2_y = 0;
+float flying_bug_3_scale = 0;
+float orange_circle_rotate = 0;
+
+GLuint flying_bug_TextureID;
+GLuint orange_circle_TextureID;
+
+GLuint LoadTexture(const char* filePath) {
+    int w, h, n;
+    unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+    if (image == NULL) {
+        std::cout << "Unable to load image. Make sure the path is correct\n";
+        assert(false);
+    }
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    stbi_image_free(image);
+    return textureID;
+}
+
+void Initialize() {
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("2D Scene", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                      640, 480, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
 
 #ifdef _WINDOWS
-	glewInit();
+    glewInit();
 #endif
 
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glViewport(0, 0, 640, 480);
 
-	SDL_Event event;
-	while (gameIsRunning) {
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-				gameIsRunning = false;
-			}
-		}
+    program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
 
-		glClear(GL_COLOR_BUFFER_BIT);
-		SDL_GL_SwapWindow(displayWindow);
-	}
+    viewMatrix = glm::mat4(1.0f);
+    modelMatrix = glm::mat4(1.0f);
+    projectionMatrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 
-	SDL_Quit();
-	return 0;
+    program.SetProjectionMatrix(projectionMatrix);
+    program.SetViewMatrix(viewMatrix);
+
+    glUseProgram(program.programID);
+
+    glClearColor(0.88f, 0.55f, 0.24f, 1.0f);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    flying_bug_TextureID = LoadTexture("flying bug.png");
+    orange_circle_TextureID = LoadTexture("orange circle.png");
+}
+
+void ProcessInput() {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+            gameIsRunning = false;
+        }
+    }
+}
+
+float lastTicks = 0.0f;
+
+void Update() {
+    float ticks = (float)SDL_GetTicks() / 1000.0f;
+    float deltaTime = ticks - lastTicks;
+    lastTicks = ticks;
+    flying_bug_1_x += 1.0f * deltaTime;
+    flying_bug_1_y += 1.0f * deltaTime;
+    flying_bug_2_x += -1.0f * deltaTime;
+    flying_bug_2_y -= -1.0f * deltaTime;
+    flying_bug_1_matrix = glm::translate(modelMatrix, glm::vec3(flying_bug_1_x, flying_bug_1_y, 0.0f));
+    flying_bug_2_matrix = glm::translate(modelMatrix, glm::vec3(flying_bug_2_x, flying_bug_2_y, 0.0f));
+
+    flying_bug_3_scale += 1.0f * deltaTime;
+    flying_bug_3_matrix = glm::scale(modelMatrix, glm::vec3(flying_bug_3_scale, flying_bug_3_scale, 1.0f));
+
+    orange_circle_rotate += 90.0f * deltaTime;   
+    orange_circle_matrix = glm::rotate(modelMatrix, glm::radians(orange_circle_rotate),
+                                       glm::vec3(0.0f, 0.0f, 1.0f));
+
+}
+
+void draw_flying_bug_1() {
+    program.SetModelMatrix(flying_bug_1_matrix);
+    glBindTexture(GL_TEXTURE_2D, flying_bug_TextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void draw_flying_bug_2() {
+    program.SetModelMatrix(flying_bug_2_matrix);
+    glBindTexture(GL_TEXTURE_2D, flying_bug_TextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void draw_flying_bug_3() {
+    program.SetModelMatrix(flying_bug_3_matrix);
+    glBindTexture(GL_TEXTURE_2D, flying_bug_TextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void draw_orange_circle() {
+    program.SetModelMatrix(orange_circle_matrix);
+    glBindTexture(GL_TEXTURE_2D, orange_circle_TextureID);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+void Render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+ 
+    float vertices[] = { -0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, 0.5, 0.5, -0.5, 0.5 };
+    float texCoords[] = { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0 };
+    
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(program.positionAttribute);
+    
+    glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
+    glEnableVertexAttribArray(program.texCoordAttribute);
+    
+    draw_flying_bug_1();
+    draw_flying_bug_2();
+    draw_flying_bug_3();
+    draw_orange_circle();
+
+    glDisableVertexAttribArray(program.positionAttribute);  
+    glDisableVertexAttribArray(program.texCoordAttribute);
+
+    SDL_GL_SwapWindow(displayWindow);
+}
+
+void Shutdown() {
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[]) {
+    Initialize();
+
+    while (gameIsRunning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+
+    Shutdown();
+    return 0;
 }
